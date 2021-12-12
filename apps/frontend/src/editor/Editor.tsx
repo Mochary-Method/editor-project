52d1db94b563f208c64ab650e111dc7a714cb2c7
Jonathan Beller <beller.jonathan@gmail.com>
2021-12-12 16:40:22 -0500

    paste custom elements

--- apps/frontend/src/editor/Editor.tsx
+++ apps/frontend/src/editor/Editor.tsx
@@ -1,8 +1,9 @@
 // @refresh reset // Fixes hot refresh errors in development https://github.com/ianstormtaylor/slate/issues/3477
 
 import React, { useCallback, useMemo, useState } from "react";
-import { createEditor, Descendant, BaseEditor } from "slate";
+import { createEditor, Descendant, Transforms } from "slate";
 import { withHistory, HistoryEditor } from "slate-history";
+import { jsx } from "slate-hyperscript";
 import { handleHotkeys } from "./helpers";
 
 import { Editable, withReact, Slate, ReactEditor } from "slate-react";
@@ -14,7 +15,7 @@ import { CustomLeaf, CustomText } from "./CustomLeaf";
 // https://docs.slatejs.org/concepts/12-typescript
 declare module "slate" {
   interface CustomTypes {
-    Editor: BaseEditor & ReactEditor & HistoryEditor;
+    Editor: ReactEditor & HistoryEditor;
     Element: CustomElement;
     Text: CustomText;
   }
@@ -25,6 +26,103 @@ interface EditorProps {
   placeholder?: string;
   onChange: (value: Descendant[]) => void;
 }
+
+const ELEMENT_TAGS = {
+  A: (el: HTMLElement) => ({ type: "link", url: el.getAttribute("href") }),
+  BLOCKQUOTE: () => ({ type: "quote" }),
+  H1: () => ({ type: "heading-one" }),
+  H2: () => ({ type: "heading-two" }),
+  H3: () => ({ type: "heading-three" }),
+  H4: () => ({ type: "heading-four" }),
+  H5: () => ({ type: "heading-five" }),
+  H6: () => ({ type: "heading-six" }),
+  IMG: (el: HTMLElement) => ({ type: "image", url: el.getAttribute("src") }),
+  LI: () => ({ type: "list-item" }),
+  OL: () => ({ type: "numbered-list" }),
+  P: () => ({ type: "paragraph" }),
+  PRE: () => ({ type: "code" }),
+  UL: () => ({ type: "bulleted-list" }),
+};
+
+// COMPAT: `B` is omitted here because Google Docs uses `<b>` in weird ways.
+const TEXT_TAGS = {
+  CODE: () => ({ code: true }),
+  DEL: () => ({ strikethrough: true }),
+  EM: () => ({ italic: true }),
+  I: () => ({ italic: true }),
+  S: () => ({ strikethrough: true }),
+  STRONG: () => ({ bold: true }),
+  U: () => ({ underline: true }),
+};
+
+export const deserialize = (el: any): any => {
+  if (el.nodeType === 3) {
+    return el.textContent;
+  } else if (el.nodeType !== 1) {
+    return null;
+  } else if (el.nodeName === "BR") {
+    return "\n";
+  }
+
+  const { nodeName } = el;
+  let parent = el;
+
+  if (
+    nodeName === "PRE" &&
+    el.childNodes[0] &&
+    el.childNodes[0].nodeName === "CODE"
+  ) {
+    parent = el.childNodes[0];
+  }
+  let children = Array.from(parent.childNodes).map(deserialize).flat();
+
+  if (children.length === 0) {
+    children = [{ text: "" }];
+  }
+
+  if (el.nodeName === "BODY") {
+    return jsx("fragment", {}, children);
+  }
+
+  if (ELEMENT_TAGS[nodeName]) {
+    const attrs = ELEMENT_TAGS[nodeName](el);
+    return jsx("element", attrs, children);
+  }
+
+  if (TEXT_TAGS[nodeName]) {
+    const attrs = TEXT_TAGS[nodeName](el);
+    return children.map((child) => jsx("text", attrs, child));
+  }
+
+  return children;
+};
+
+const withHtml = (editor) => {
+  const { insertData, isInline, isVoid } = editor;
+
+  editor.isInline = (element) => {
+    return element.type === "link" ? true : isInline(element);
+  };
+
+  editor.isVoid = (element) => {
+    return element.type === "image" ? true : isVoid(element);
+  };
+
+  editor.insertData = (data) => {
+    const html = data.getData("text/html");
+
+    if (html) {
+      const parsed = new DOMParser().parseFromString(html, "text/html");
+      const fragment = deserialize(parsed.body);
+      Transforms.insertFragment(editor, fragment);
+      return;
+    }
+
+    insertData(data);
+  };
+
+  return editor;
+};
 
 export const Editor: React.FC<EditorProps> = ({
   initialValue = [],
@@ -37,7 +135,10 @@ export const Editor: React.FC<EditorProps> = ({
     []
   );
   const renderLeaf = useCallback((props) => <CustomLeaf {...props} />, []);
-  const editor = useMemo(() => withHistory(withReact(createEditor())), []);
+  const editor = useMemo(
+    () => withHtml(withHistory(withReact(createEditor()))),
+    []
+  );
 
   return (
     <Slate
